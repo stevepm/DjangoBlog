2c9ff2499e10f86fd41c17cf1b479a155ea181bd
Steven Magelowitz <steven.magelowitz@gmail.com>
2012-01-29 15:49:43 -0800

    blog added

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Post(models.Model):
+	title = models.CharField(max_length=60)
+	body = models.TextField()
+	created = models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.title
+
+### ADMIN
+
+class PostAdmin(admin.ModelAdmin):
+	search_fields = ["title"]
+
+admin.site.register(Post, PostAdmin)
